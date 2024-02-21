db46cbf06dfb6e614f890a7cbd0b6f9501653176
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-02-20 21:28:24 -0600

    Add files via upload

---
+++ Ejercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int var1 = 10;
+    int var2 = 20;
+    int var3 = 2;
+    int res = var1 * var2 * var3;
+
+    printf("El resultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res);
+    printf("\n");
+    printf("El resultado es: %d", res);
+    printf("\n");
+    return 0;
+}
---
+++ Ejercicio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    double var1 = 31.5;
+    double var2 = 10.5;
+    double res = var1 / var2;
+
+    printf("El resultado de dividir %f entre %f es: %f", var1 , var2 , res);
+    printf("\n");
+    printf("El resultado es: %f", res);
+    printf("\n");
+    return 0;
+}
