87b193f2a01c0b8e8a3c5dd7da096b8f4172de94
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-02-29 22:08:55 -0600

    Add files via upload

---
+++ n5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main (){
+    int b,res;
+
+    puts("area de un cuadrado;\n");
+    puts("Dame el valor de la base;\n");
+    scanf("%d", &b);
+    res=b*b;
+
+    printf("El area del cuadrado es;%d\n",res);
+
+    return 0;
+
+ }
