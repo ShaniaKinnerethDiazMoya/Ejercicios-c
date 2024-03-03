5c8fa51a57c984a4b1840d8bdecd51a6e2950585
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-03-02 20:55:25 -0600

    Add files via upload

---
+++ n12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+main( )
+{
+int expresión, x = 1;
+expresion = ! ( (1 == x) && 4)
+if (expresión )
+printf(“El resultado de la expresión es verdadero”);
+else
+printf(“El resultado de la expresion es falso”);
+x = 4;
+for( ; x < 10; x += 3);
+printf(“%d”, x / 2);
+x = 10;
+do
+{
+printf(“%d”, x--);
+} while ( x >5);
+}
