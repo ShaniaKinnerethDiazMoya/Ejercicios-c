89c0718ae92f741254f5b910daa98cd1e50c687d
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-03-03 22:01:20 -0600

    Add files via upload

---
+++ n18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ( void ){
+    int a =10 , b =3 , c ;
+    float w =2.0 , x =3.77 , y , z ;
+
+    c = w - x ; // La resta es -1 de tipo entero
+    y = a + b ; // La suma es 13.0 de tipo flotante
+    z = a * x ; // La multiplicacion es 37.7 de tipo flotante
+
+    return 0;
+}
