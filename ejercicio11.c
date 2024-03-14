3680b684d9fcd62376d8abe96f7624cf4cc87dd2
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-03-13 22:35:37 -0600

    Add files via upload

---
+++ ejercicio11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 0, n2;
+    while(numero<=997){
+        n2=numero+3;
+        printf("%d\n", n2);
+        numero=numero+3;
+    }
+    return 0;
+}
