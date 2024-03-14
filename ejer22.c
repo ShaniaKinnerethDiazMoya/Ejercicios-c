aad1bf96140d5f9d82bdf7609ea4ed7042661249
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-03-13 22:36:42 -0600

    Add files via upload

---
+++ ejer22.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 1;
+    while(numero<=1000){
+        
+        if((numero%7)!=0){
+            if((numero%2)!=0){
+                
+                printf("%d\n", numero-1);
+            }
+        }
+        numero=numero+7;
+    }
+    return 0;
+}
