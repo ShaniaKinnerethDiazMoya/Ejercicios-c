e52fdf97a4d2564b687cd7ca7e97fbefafadba18
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-11-15 21:32:03 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -5,6 +5,7 @@ int main(){
     double var2 = 10.5;
     double res = var1 / var2;
 
+    
     printf("El resultado de dividir %f entre %f es: %f", var1 , var2 , res);
     printf("\n");
     printf("El resultado es: %f", res);
