7e56e2b63d112ec460124e0817cbda2e9b29611e
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-11-18 20:53:54 -0600

    Update n16.c

--- n16.c
+++ n16.c
@@ -1,6 +1,7 @@
 # include <stdio.h>
 
 int main ( void ) {
+    
     typedef int ENTERO ;
     float valor_f1 = 1.2389 , valor_f2 = 5.4321 , sumaf ;
     ENTERO valor_i1 = 5 , valor_i2 = 7 , sumai ;
