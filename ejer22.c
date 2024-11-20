58cb95d43267e5a3432892f72dca8ed45b670151
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-11-19 21:35:21 -0600

    Update ejer22.c

--- ejer22.c
+++ ejer22.c
@@ -1,10 +1,8 @@
-
 #include <stdio.h>
 
 int main(){
     int numero = 1;
-    while(numero<=1000){
-        
+    while(numero<=1000){   
         if((numero%7)!=0){
             if((numero%2)!=0){
                 
