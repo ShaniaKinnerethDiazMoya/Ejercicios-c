cea5b9460ff218328cad5da783d0f31bfc377026
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-12-04 21:31:58 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -6,6 +6,7 @@ int main (){
     int var2 = 20;
     int var3 = 2;
     int res = var1 * var2 * var3;
+    
     printf("El resultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res);
     printf("\n");
     printf("El resultado es: %d", res);
